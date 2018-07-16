f26bf00c95d3c209b57d78f9a6ea1b933ec7c063
Sergio <dev01@hlx.co>
2018-07-16 00:02:32 -0400

    Update ShipEngineAPI.py

--- ShipEngineAPI.py
+++ ShipEngineAPI.py
@@ -5,6 +5,13 @@ class ShipEngineAPI():
 	def __init__(self, key):
 		self.API_KEY = key
 		self.API_ENDPOINT = 'https://api.shipengine.com/v1/tracking'
+		self.__author__ = "Sergio Molanes"
+		self.__copyright__ = "Copyright 2018, HLX Studios"
+		self.__license__ = "GPL 3.0"
+		self.__version__ = "1.0"
+		self.__maintainer__ = "Sergio Molanes"
+		self.__email__ = "sergio@hlx.co"
+		self.__status__ = "Production"
 
 	def trackPackage(self, carrier_code, tracking_number):
 		headers = {
